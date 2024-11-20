71f355d46203a5a3fb1fba15398186fa9adb2918
carlsscott12 <carlsscott12@gmail.com>
2024-11-20 15:20:07 -0600

    Done

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true
-})
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
+  outputDir: 'dist',
+  devServer: {
+    port: 8080, // Specify your preferred port
+    open: true, // Automatically open the browser on server start
+  },
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': require('path').resolve(__dirname, 'src'),
+      },
+    },
+  },
+};
+
